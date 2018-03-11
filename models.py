2d0e3ed89eba95e4ed75747e1324504fc1c41a26
Chris Dinant <chris.dinant@gmail.com>
2018-03-11 14:36:14 +0100

    Update models.py

--- models.py
+++ models.py
@@ -92,8 +92,8 @@ class ResNet():
     
 #%%
 def ctc_lambda_func(args):
-        y_pred, labels, input_length, label_length = args    
-        return K.ctc_batch_cost(labels, y_pred, input_length, label_length)    
+    y_pred, labels, input_length, label_length = args    
+    return K.ctc_batch_cost(labels, y_pred, input_length, label_length)    
     
         
 #%%
